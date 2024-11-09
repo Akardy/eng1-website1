ee316485cb47af216d72c88bede7d7cc1ad35d23
Akardy <73198062+Akardy@users.noreply.github.com>
2024-11-09 22:07:32 +0000

    updated the urls in Links

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,12 +4,12 @@ function Links() {
   return (
     <div class="block">
         <h1>Links:</h1>
-        <p><a href="#" target="_blank">Download the Game</a></p>
-        <p><a href="#" target="_blank">GitHub Repository</a></p>
-        <p><a href="#" target="_blank">Requirements</a></p>
-        <p><a href="#" target="_blank">Architecture</a></p>
-        <p><a href="#" target="_blank">Method Selection/Planning</a></p>
-        <p><a href="#" target="_blank">Risk Assessment/Mitigation</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Download the Game</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">GitHub Repository</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Requirements</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Architecture</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Method Selection/Planning</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Risk Assessment/Mitigation</a></p>
      
     </div>
   );
