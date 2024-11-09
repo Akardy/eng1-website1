049b87125c7b024198cfccbc0e8f0be7dd155c7a
Akardy <73198062+Akardy@users.noreply.github.com>
2024-11-09 21:59:34 +0000

    replacing <a> with Link Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -6,9 +7,9 @@ function Header() {
             <nav>
                 <ul>
                     <img id="logo" src="./coffee.png" alt="a cafe building" />
-                    <li><a href="./">Home</a></li>
-                    <li><a href="./team">Team</a></li>
-                    <li><a href="./team">Credits</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/team">Team</Link></li>
+                    <li><Link to="/credits">Credits</Link></li>
                 </ul>
             </nav>
             <button>Download the Game</button>
