1fff6b1cb8cb755bcef068d066933c9b4a681c14
Akardy <73198062+Akardy@users.noreply.github.com>
2024-11-09 22:09:58 +0000

    fixed a bug

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,12 +4,12 @@ function Links() {
   return (
     <div class="block">
         <h1>Links:</h1>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Download the Game</a></p>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">GitHub Repository</a></p>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Requirements</a></p>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Architecture</a></p>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Method Selection/Planning</a></p>
-        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank">Risk Assessment/Mitigation</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">Download the Game</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">GitHub Repository</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">Requirements</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">Architecture</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">Method Selection/Planning</a></p>
+        <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noopener">Risk Assessment/Mitigation</a></p>
      
     </div>
   );
