46eaf414768317bec74694a9ec7475982a295a8e
Akardy <73198062+Akardy@users.noreply.github.com>
2024-11-10 19:25:47 +0000

    refactoring the code

--- src/components/Links.js
+++ src/components/Links.js
@@ -8,6 +8,7 @@ function Links() {
         <a href="https://github.com/Akardy/Eng1-Assessment-1" target="_blank" rel="noreferrer">GitHub Repository</a>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Requirements</a>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Architecture</a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Implementation</a>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Method Selection/Planning</a>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Risk Assessment/Mitigation</a>
      
--- src/pages/WeeklySnapshots.js
+++ src/pages/WeeklySnapshots.js
@@ -8,8 +8,19 @@ function WeeklySnapshots() {
         <Header /> 
         <div id="hero">
             <main class="weekly-snapshots">
-                <h1>Weekly Snapshots:</h1>
-                <img id="snapshots-image" src='./weekly.png' alt='weekly snapshots' />
+                <h1>Weekly Snapshots</h1>
+                <h3>Week 2:</h3>
+                <img id="snapshots-image" src='./week2.png' alt='week 2 snapshot' />
+                <h3>Week 3:</h3>
+                <img id="snapshots-image" src='./week3.png' alt='week 3 snapshot' />
+                <h3>Week 4:</h3>
+                <img id="snapshots-image" src='./week4.png' alt='week 4 snapshot' />
+                <h3>Week 5:</h3>
+                <img id="snapshots-image" src='./week5.png' alt='week 5 snapshot' />
+                <h3>Consolidation Week:</h3>
+                <img id="snapshots-image" src='./week5.1.png' alt='week 5.1 snapshot' />
+                <h3>Week 6:</h3>
+                <img id="snapshots-image" src='./week6.png' alt='week 6 snapshot' />
             </main>
             <Links />
         </div>  
