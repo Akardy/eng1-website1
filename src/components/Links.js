32eb9c2276cc7f1cb7d9e8107085e2d227033e8d
Akardy <73198062+Akardy@users.noreply.github.com>
2024-11-10 19:50:33 +0000

    updated the links

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,11 +6,11 @@ function Links() {
         <h1>Links:</h1>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Download the Game</a>
         <a href="https://github.com/Akardy/Eng1-Assessment-1" target="_blank" rel="noreferrer">GitHub Repository</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Requirements</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Architecture</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Implementation</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Method Selection/Planning</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer">Risk Assessment/Mitigation</a>
+        <a href="https://drive.google.com/file/d/13G6af1zbhaopx5_hZyeZ0NdNmvQeKyB3/view?usp=sharing" target="_blank" rel="noreferrer">Requirements</a>
+        <a href="https://drive.google.com/file/d/1dA1YFTi32XpJ2t2QN57JBeyop8NG_dXa/view?usp=sharing" target="_blank" rel="noreferrer">Architecture</a>
+        <a href="https://drive.google.com/file/d/1ENrLFm241IOE5qyz8Ktl5MLKhSb7UHYX/view?usp=sharing" target="_blank" rel="noreferrer">Implementation</a>
+        <a href="https://drive.google.com/file/d/1jn11RrU96cLeZqUXqgNaX9dCPtwbW_zC/view?usp=sharing" target="_blank" rel="noreferrer">Method Selection/Planning</a>
+        <a href="https://drive.google.com/file/d/1Yhp2J3tJWMfPYx4masxOsn-ZJ0P1r5mD/view?usp=sharing" target="_blank" rel="noreferrer">Risk Assessment/Mitigation</a>
      
     </div>
   );
--- src/pages/DesignProgress.js
+++ src/pages/DesignProgress.js
@@ -20,7 +20,7 @@ function DesignProgress() {
               <h3>Second Structural Diagram (Toward Modularity)</h3>
               <img id="snapshots-image" src='./diagram3.png' alt='final diagram' />
               <p>To read more about the project architecture, please check this document: 
-                  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" target="_blank" rel="noreferrer"> Click here</a>
+                  <a href="https://drive.google.com/file/d/1dA1YFTi32XpJ2t2QN57JBeyop8NG_dXa/view?usp=sharing" target="_blank" rel="noreferrer"> Click here</a>
               </p>
             </main>
             <Links />
